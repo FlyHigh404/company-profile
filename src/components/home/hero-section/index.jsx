d7bdd96a048278b490bfa302dee8f152924ae850
renaramarosalinda <21081010190@student.upnjatim.ac.id>
2025-06-04 03:47:05 +0700

    feat(hero): add id

--- src/components/home/hero-section/index.jsx
+++ src/components/home/hero-section/index.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 export default function HeroSection() {
 	return (
-	<div className=" px-[20px] md:px-[80px] lg:px-[80px]">
+	<section className=" px-[20px] md:px-[80px] lg:px-[80px]">
 		<div className="flex flex-col items-center justify-center text-center">
 			<h1 className="typo-h3 md:typo-h2 lg:typo-h1  leading-none bg-gradient-to-r from-[#EF9419] to-[#C94F1E] text-transparent bg-clip-text mt-10 py-5 md:py-6 lg:py-8 ">Kami mengembangkan solusi digital  untuk bisnis anda</h1>
 			<div className="py-4 md:py-6 lg:py-8">
@@ -13,6 +13,6 @@ export default function HeroSection() {
 				<a href="#"><span className="typo-b-sm md:typo-b-md lg:typo-b-md font-[600] bg-gradient-to-r from-[#EF9419] to-[#C94F1E] p-2.5 md:p-4 px-6 md:px-10  text-neutral-50 rounded-[30px]">Cari Tahu Solusi Kami</span></a>
 			</div>
 		</div>
-	</div>
+	</section>
 	);
 }
