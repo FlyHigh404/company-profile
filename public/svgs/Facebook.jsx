758e3815adc3fe88f00d0178f4918a113405a3ca
renaramarosalinda <21081010190@student.upnjatim.ac.id>
2025-06-08 18:50:04 +0700

    temp

--- public/svgs/Facebook.jsx
+++ public/svgs/Facebook.jsx
@@ -1,6 +1,6 @@
 export default function Facebook(){
     return(
-        <div>
+        <div className="pr-2">
             <svg width="16" height="25" viewBox="0 0 16 25" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M14.75 1.8606H11.0682C9.44072 1.8606 7.87991 2.41936 6.72912 3.41398C5.57833 4.40859 4.93182 5.75757 4.93182 7.16417V10.3463H1.25V14.5892H4.93182V23.0749H9.84091V14.5892H13.5227L14.75 10.3463H9.84091V7.16417C9.84091 6.88285 9.97021 6.61305 10.2004 6.41413C10.4305 6.21521 10.7427 6.10345 11.0682 6.10345H14.75V1.8606Z" fill="#262626" stroke="#262626" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
         </svg>
--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -7,16 +7,16 @@ import ArrowToTop from '../../../public/svgs/ArrowToTop'
 export default function Footer() {
   return (
     <footer>
-      <div className="flex items-center justify-between mx-15 mt-[67px]">
+      <div className="flex flex-wrap justify-between mx-5 md:mx-8 lg:mx-20 ">
         <div className="flex flex-col">
-          <p className="typo-h2 md:typo-h2 lg:logo-footer leading-[0.8] text-start  bg-gradient-to-r from-[#EF9419] to-[#C94F1E] text-transparent bg-clip-text pb-5 md:pb-10 lg:pb-12">FlyHigh</p>
-          <p className="typo-b-sm md:typo-b-md lg:typo-b-md font-[500] text-neutral-700 cursor-default pb-5 md:pb-10 lg:pb-12">© Copyright {new Date().getFullYear()} by Fly High</p>
+          <p className="logo-footer leading-[0.8] text-start  bg-gradient-to-r from-[#EF9419] to-[#C94F1E] text-transparent bg-clip-text pb-5 md:pb-10 lg:pb-12">FlyHigh</p>
+          <p className="text-lg font-[500] text-neutral-700 cursor-default pb-5 md:pb-10 lg:pb-12">© Copyright {new Date().getFullYear()} by Fly High</p>
         </div>
 
-        <div className="typo-b-sm md:typo-b-md lg:typo-b-md text-neutral-700 ">
-          <div className="flex flex-wrap lg:justify-center md:gap-8 lg:gap-10">
-            <div className="w-1/3 md:w-1/6 lg:w-1/5 space-y-2 lg:space-y-5 ">
-              <p className="typo-b-lg font-bold">Navigasi</p>
+        <div className="text-lg text-neutral-700 ">
+          <div className="flex lg:justify-center md:gap-8 lg:gap-x-24">
+            <div className="w-2/5 md:w-1/6 lg:w-3/5 space-y-2 lg:space-y-5 ">
+              <p className="text-lg font-bold">Navigasi</p>
               <p>
                 <a href="#">Tentang Kami</a>
               </p>
@@ -31,8 +31,8 @@ export default function Footer() {
               </p>
             </div>
 
-            <div className="w-2/3 md:w-3/6 lg:w-2/5 space-y-2 lg:space-y-5 cursor-default">
-              <p className="typo-b-lg font-bold">
+            <div className="w-2/5 md:w-3/6 lg:w-2/5 space-y-2 lg:space-y-5 cursor-default">
+              <p className="text-lg font-bold">
                 <a href="">Hubungi Kami</a>
               </p>
               <p>
@@ -41,27 +41,29 @@ export default function Footer() {
               <p>+62 851-4116-8042</p>
             </div>
 
-            <div className="w-full md:w-1/6 lg:w-1/5 flex flex-row md:flex-col lg:flex-col items-center gap-5 md:gap-2 lg:gap-5 pt-5 md:pt-0 lg:pt-0">
-              <a href="https://www.linkedin.com/company/cv-flyhigh-sinergi-indonesia">
-                <Linkedin />
-              </a>
-              <a href="">
-                <Facebook />
-              </a>
-              <a href="">
-                <Twitter />
-              </a>
-              <a href="https://www.instagram.com/flyhighcorp_/">
-                <Instagram />
-              </a>
+            <div className="w-1/1 md:w-1/6 lg:w-1/5 ">
+              <div className='flex flex-row md:flex-col lg:flex-col items-center gap-5 md:gap-2 lg:gap-5 pt-5 md:pt-0 lg:pt-0'>
+                <a href="https://www.linkedin.com/company/cv-flyhigh-sinergi-indonesia">
+                  <Linkedin />
+                </a>
+                <a href="">
+                  <Facebook />
+                </a>
+                <a href="">
+                  <Twitter />
+                </a>
+                <a href="https://www.instagram.com/flyhighcorp_/">
+                  <Instagram />
+                </a>
+              </div>
             </div>
           </div>
         </div>
       </div>
       <a href="#hero">
-        <div className="flex flex-col justify-center items-center gap-2">
+        <div className="flex flex-col justify-center items-center gap-2 py-10">
           <ArrowToTop />
-          <p className="typo-b-lg text-neutral-700 font-semibold">Kembali Ke Atas</p>
+          <p className="text-lg text-neutral-700 font-semibold ">Kembali Ke Atas</p>
         </div>
       </a>
     </footer>
